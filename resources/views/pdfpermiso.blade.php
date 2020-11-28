fe060e16dabcf06bfec221f96d4778558a10db5b
Angel_Salazar <angel_salazar2017@hotmail.com>
2020-11-28 17:51:50 -0600

    Vista del PDF (API-3)

--- resources/views/pdfpermiso.blade.php
+++ resources/views/pdfpermiso.blade.php
@@ -8,20 +8,101 @@
         <p>Logos encabezado</p>
     </header>
     <div class="container">
-        <h1>Permiso para realizar maniobras de carga y descarga</h1>
+        <p class="tittle">PERMISO PARA REALIZAR MANIOBRAS DE CARGA Y DESCARGA</p>
+        <div class="container">
+            <div class="folio texto border border-3 border-dark">Permiso: 0000001</div>
+            <div class="container">
+                <p class="texto">A QUIEN CORRESPONDA:</p>
+                <p class="texto">Por medio de la precente la Dirección de Tráncito, concede permiso al vehículo de las siguientes características:</p>
+                
+                <table class="table table-bordered texto">
+                    <thead>
+                      
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>First</td>
+                            <td>Last</td>
+                            <td>Handle</td>
+                          </tr>
+                        <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">Jacob</td>
+                            <td>Thornton</td>
+                        </tr>
+                        <tr>
+                            <td colspan="3">Larry the Bird</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <p class="texto">Para que pueda realizar manobras de carga y descarga en el primer cuadro de la ciudad.</p>
+
+                <table class="table texto table-bordered">
+                    <tbody>
+                        <tr>
+                            <td colspan="3">First</td>
+                        </tr>
+                        <tr>
+                            <td colspan="3">Mark</td>
+                        </tr>
+                        <tr>
+                            <td colspan="3">Jacob</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <p class="texto font-weight-bold">QUEDA ESTRICTAMENTE PROHIBIDO ESTACIONARSE SOBRE LAS AVENIDAS:</p>
+                <ol class="texto font-weight-bold">
+                    <li>ÁLVARO OBREGÓN</li>
+                    <li>HÉROES</li>
+                </ol>
+
+                <p class="texto text-justify">Así mismo se le exhorta para que cumpla con lo establecido en el reglamento de Tránsito del Estado de Quintana
+                    Roo. Especialmente con lo dispuesto en los artículos 75, 90, 94, 222 y 228, siendo que de no cumplir con estas
+                    disposiciones será acreedor a la sanción correspondiente y cancelación del permiso.</p>
+                <p class="texto text-justify">Por lo anterior tendrá la obligación de utilizar las medidas de seguridad necesarias (conos) para brindar protección
+                    a su personal y a los usuarios de la vía pública. </p>
+                <p class="texto text-justify    "><b>NOTA: </b>Este permiso no exime de responsabilidad al conductor, en caso de verse involucrado en un hecho de
+                    tránsito. </p>
+            </div>
+        </div>
     </div>
     <footer>
         <p>Logos pie de pagina</p>
     </footer>
 
     <style>
+       
         @page{
             margin: 0px 0px;
-            font-size: 1em;
+            
         }
 
         body{
-            margin: 3cm 2cm 2cm;
+            margin: 3cm 0.5cm 0.5cm;
+            font-family: 'Open Sans', sans-serif;
+        }
+
+        .tittle{
+            text-align: center;
+            font-size: 14px;
+        }
+
+        .texto{
+            font-size: 13px;
+        }
+
+        .folio{
+            float: right;
+            text-align: center;
+            width: 140px;
+            margin: 0cm;
+            padding: 3px 0px;
         }
 
         header{
@@ -29,7 +110,7 @@
             top: 0cm;
             left: 0cm;
             right: 0cm;
-            height: 2cm;
+            height: 2.5cm;
             background-color: lightblue;
             color: white;
             text-align: center;
